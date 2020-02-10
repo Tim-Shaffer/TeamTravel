3f7a50715766e90b7c4dd2923935b4d82d85f3ee
Tim-Shaffer <tcshaf24@ptd.net>
2020-02-10 17:37:12 -0500

    started work on the function to load the team list based on the selected league

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,3 +1,30 @@
+// Global variables for league arrays of teams
+
+// alphabetical array of MLB Teams
+let mlbArray = ["Arizona Diamondbacks", "Atlanta Braves", "Baltimore Orioles", "Boston Red Sox", "Chicago Cubs", "Chicago White Sox", "Cincinnati Reds", "Cleveland Indians",
+"Colorado Rockies", "Detroit Tigers", "Houston Astros", "Kansas City Royals", "Los Angeles Angels", "Los Angeles Dodgers", "Miami Marlins", "Milwaukee Brewers", "Minnesota Twins",
+"New York Mets", "New York Yankees", "Oakland Athletics", "Philadelphia Phillies", "Pittsburgh Pirates", "San Diego Padres", "San Francisco Giants", "Seattle Mariners", "St. Louis Cardinals",
+"Tampa Bay Rays", "Texas Rangers", "Toronto Blue Jays", "Washington Nationals"];
+
+// alphabetical array of NBA Teams
+let nbaArray = ["Atlanta Hawks", "Boston Celtics", "Brooklyn Nets", "Charlotte Hornets", "Chicago Bulls", "Cleveland Cavaliers", "Dallas Mavericks", "Denver Nuggets", "Detroit Pistons",
+"Golden State Warriors", "Houston Rockets", "Indiana Pacers", "LA Clippers", "LA Lakers", "Memphis Grizzlies", "Miami Heat", "Milwaukee Bucks", "Minnesota Timberwolves", "New Orleans Pelicans",
+"New York Knicks", "Oklahoma City Thunder", "Orlando Magic", "Philadelphia Sixers", "Phoenix Suns", "Portland Trail Blazers", "Sacramento Kings", "San Antonio Spurs", "Toronto Raptors",
+"Utah Jazz", "Washington Wizards"];
+
+// alphabetical array of NFL Teams
+let nflArray = ["Atlanta Falcons", "Arizona Cardinals", "Baltimore Ravens", "Buffalo Bills", "Carolina Panthers", "Chicago Bears", "Cincinnati Bengals", "Cleveland Browns", "Dallas Cowboys",
+"Denver Broncos", "Detroit Lions", "Green Bay Packers", "Houston Texans", "Indianapolis Colts", "Jacksonville Jaguars", "Kansas City Chiefs", "Los Angeles Chargers", "Los Angeles Rams",
+"Miami Dolphins", "Minnesota Vikings", "New England Patriots", "New Orleans Saints", "NY Giants", "NY Jets", "Oakland Raiders", "Philadelphia Eagles", "Pittsburgh Steelers", "San Francisco 49ers",
+"Seattle Seahawks", "Tampa Bay Buccaneers", "Tennessee Titans", "Washington Redskins"];
+
+// alphabetical array of NHL Teams
+let nhlArray = ["Anaheim Ducks", "Boston Bruins", "Buffalo Sabres", "Calgary Flames", "Carolina Hurricanes", "Chicago Blackhawks", "Colorado Avalanche", "Columbus Blue Jackets", "Dallas Stars",
+"Detroit Red Wings", "Edmonton Oilers", "Florida Panthers", "Las Vegas Golden Knights", "Los Angeles Kings", "Minnesota Wild", "Montreal Canadiens", "Nashville Predators", "New Jersey Devils", 
+"New York Islanders", "New York Rangers", "Ottawa Senators", "Philadelphia Flyers", "Phoenix Coyotes", "Pittsburgh Penguins", "San Jose Sharks", "St. Louis Blues", "Tampa Bay Lightning", 
+"Toronto Maple Leafs", "Vancouver Canucks", "Washington Capitals", "Winnipeg Jets"];
+
+// --------------------------------------------------------------------------------------
 // event listener for submit button to get user name.
 $('#submitButton').on('click', function(event) {
 
@@ -11,3 +38,48 @@ $('#submitButton').on('click', function(event) {
     // add the user to local storage.
     localStorage.setItem("username", userName);
 });
+// --------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------
+//  function to populate the list of teams based on the selected league
+//  Parameter values:
+//  league - the value provided from the user selection of the radio-button (MLB, NBA, NFL, NHL)
+// --------------------------------------------------------------------------------------
+function loadTeamSelectList(league) {
+
+    // verify the passed in league is valid
+    if (league === "MLB" || league === "NBA" || league === "NFL" || league === "NHL") {
+
+        let array = [];
+        // set the array for the appropriate league
+        if (league === "MLB") {
+            array = mlbArray;
+        } else if (league === "NBA") {
+            array = nbaArray;
+        } else if (league === "NFL") {
+            array = nflArray;
+        } else if (league === "NHL") {
+            array = nhlArray;
+        };
+
+        // basics found on https://www.caveofprogramming.com/guest-posts/introduction-to-jquery-populating-creating-dynamic-dropdowns.html
+        // remove any previously added options
+        $("#team-dropdown").find('option').remove();
+        
+        // loop through the array and build the new list
+        for (i=0; i < array.length; i++) {
+            $("#dropdown").append('<option value="'+ array[i]+'">'+ array[i] +'</option>');
+        };  
+        
+        return true;
+
+    } else {
+
+        return false;
+
+    }
+
+}
+// --------------------------------------------------------------------------------------
+// end of loadTeamSelectList() function
+// --------------------------------------------------------------------------------------
